dfe0a9ed3209fdbbae004e626b16ea0d7a9cac4b
Angel Romero <mrangelrcd@gmail.com>
2023-03-30 17:31:24 +0200

    Game functional.

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const Player = (mark) => {
+const Player = (marka) => {
+  const mark = marka;
+
   const makeMove = (index) => {
     Board.addMove(mark, index);
   };
-  return { makeMove };
+
+  const win = () => {
+    displayController.displayWinScreen(mark);
+  };
+  return { mark, makeMove, win };
 };
 
 const xPlayer = Player("X");
@@ -13,10 +19,6 @@ const Board = (function () {
 
   let _currentPlayer = xPlayer;
 
-  const getCurrentPlayer = () => {
-    return _currentPlayer;
-  };
-
   const _togglePlayer = () => {
     if (_currentPlayer === xPlayer) {
       _currentPlayer = oPlayer;
@@ -25,12 +27,68 @@ const Board = (function () {
     }
   };
 
+  const _checkWinner = () => {
+    if (
+      (_board[0] &&
+        _board[1] &&
+        _board[2] &&
+        _board[0] === _board[1] &&
+        _board[0] === _board[2]) ||
+      (_board[3] &&
+        _board[4] &&
+        _board[5] &&
+        _board[3] === _board[4] &&
+        _board[3] === _board[5]) ||
+      (_board[6] &&
+        _board[7] &&
+        _board[8] &&
+        _board[6] === _board[7] &&
+        _board[6] === _board[8]) ||
+      (_board[0] &&
+        _board[3] &&
+        _board[6] &&
+        _board[0] === _board[3] &&
+        _board[0] === _board[6]) ||
+      (_board[1] &&
+        _board[4] &&
+        _board[7] &&
+        _board[1] === _board[4] &&
+        _board[1] === _board[7]) ||
+      (_board[2] &&
+        _board[5] &&
+        _board[8] &&
+        _board[2] === _board[5] &&
+        _board[2] === _board[8]) ||
+      (_board[0] &&
+        _board[4] &&
+        _board[8] &&
+        _board[0] === _board[4] &&
+        _board[0] === _board[8]) ||
+      (_board[2] &&
+        _board[4] &&
+        _board[6] &&
+        _board[2] === _board[4] &&
+        _board[2] === _board[6])
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  const getCurrentPlayer = () => {
+    return _currentPlayer;
+  };
+
   const addMove = (move, index) => {
     if (_board.at(index)) {
       return;
     }
 
     _board.splice(index, 1, move);
+    displayController.drawMarkInCell(index, _currentPlayer);
+    if (_checkWinner()) {
+      _currentPlayer.win();
+    }
     _togglePlayer();
   };
 
@@ -50,7 +108,16 @@ const Board = (function () {
 })();
 
 const displayController = (() => {
+  const restartButton = document.querySelector("#win-screen button");
+  const winnerDisplay = document.querySelector(".winner");
+  const winScreen = document.querySelector("#win-screen");
   const _cells = document.querySelectorAll("#board-container div");
+
+  restartButton.addEventListener("click", () => {
+    Board.clear();
+    displayController.clearDisplay();
+    displayController.hideWinScreen();
+  });
 
   _cells.forEach((cell) => {
     cell.addEventListener("click", () => {
@@ -60,4 +127,27 @@ const displayController = (() => {
       Board.logBoard();
     });
   });
+
+  const drawMarkInCell = (index, player) => {
+    _cells[index].setAttribute("data-mark", player.mark);
+  };
+
+  const displayWinScreen = (winnerMark) => {
+    winnerDisplay.textContent = winnerMark;
+    winScreen.classList.remove("hidden");
+  };
+
+  const hideWinScreen = () => {
+    winScreen.classList.add("hidden");
+  };
+
+  const clearDisplay = () => {
+    _cells.forEach((cell) => cell.setAttribute("data-mark", ""));
+  };
+  return {
+    drawMarkInCell,
+    displayWinScreen,
+    hideWinScreen,
+    clearDisplay,
+  };
 })();
